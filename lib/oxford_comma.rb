c8ce23920c53f52b095c8a8be707f7b11dbdba2e
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-16 00:24:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,9 +6,9 @@ elsif array.length == 2
 elsif array.length == 3 
   "#{array[0]}, #{array[1]}, and #{array[2]}"
 else 
-  message = ""
+  message = " "
   array.each_with_index do |value,index|
-    message+= index < array.length-1 ? " #{value}," : " and #{value}"
+    message+= index < array.length-1 ? "#{value}," : " and #{value}"
   end
   message
 end
